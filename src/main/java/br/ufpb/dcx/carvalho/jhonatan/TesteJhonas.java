a427b6ebaa1c6900a3dec4b30ab586afad88db20
jhonatan.carvalho <jhonatan.carvalho@dcx.ufpb.br>
2025-11-11 09:19:07 -0300

    criei um exemplo de classe para demonstrar o IO

---
+++ src/main/java/br/ufpb/dcx/carvalho/jhonatan/TesteJhonas.java
@@ -0,0 +1,9 @@
+package br.ufpb.dcx.carvalho.jhonatan;
+
+public class TesteJhonas {
+    void main(){
+        IO.println("ABC");
+        String nome = IO.readln("Escrever o nome: ");
+        IO.println("Olá" + nome);
+    }
+}
